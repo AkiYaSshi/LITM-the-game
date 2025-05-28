93fe112a64a9aa3b90c6e825a8041f5e9c699e8a
AkiYaSshi <9tu4or5@gmail.com>
2025-05-28 13:06:17 +0800

    minor add
    +newgame can move to next scene with save slot selected

--- Leave it to me_DEMO01/Assets/Script/Menu/MenuSaveSelector.cs
+++ Leave it to me_DEMO01/Assets/Script/Menu/MenuSaveSelector.cs
@@ -3,6 +3,9 @@ using UnityEngine.UI;
 
 public class MenuSaveSelector : MonoBehaviour
 {
+    [Tooltip("測試需要的場合，輸入這裡之後跳到gameplay即可存檔")]
+    [SerializeField]
+    private int inputSlot;
     /// <summary>
     /// 透過呼叫save file manager 設定存檔編號
     /// </summary>
@@ -10,4 +13,8 @@ public class MenuSaveSelector : MonoBehaviour
     {
         SaveFileManager.SetSaveSlot(slot);
     }   
+    public void InpuSetSaveSlot()
+    {
+        SaveFileManager.SetSaveSlot(inputSlot);
+    }
 }
